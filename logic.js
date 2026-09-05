6904fb802ebe0cd184a340c463b3cd7770daeeea
Vaishnavi Jaiswal <vaishnavijais2609@gmail.com>
2026-09-05 11:21:09 +0530

    Added motion

--- logic.js
+++ logic.js
@@ -11,26 +11,37 @@ window.addEventListener("resize", resizecanv);
 const tom={
     x: 120,
     y: canv.height-170,
+    walking: true,
+    walkspeed: 2.5,
+    walktime:0,
     scale: 1,
     legangle: 0,
-    armangle: 0
+    armangle: 0,
+    bounce: 0
 };
+function walk(){
+    if(!tom.walking) return;
+    tom.x+=tom.walkspeed;
+    tom.walktime+=0.15;
+    tom.legangle= Math.sin(tom.walktime)*0.5;
+    tom.armangle= Math.sin(tom.walktime)*0.4;
+    tom.bounce=Math.abs(Math.sin(tom.walktime))*3;
+}
 function draw(){
     dr.save();
-    dr.translate(tom.x, tom.y);
+    dr.translate(tom.x, tom.y+tom.bounce);
     dr.scale(tom.scale, tom.scale);
-   
 
     dr.strokeStyle="white";
     dr.lineWidth=13;
     dr.lineCap="round";
     dr.beginPath();
     dr.moveTo(-13, 85);
-    dr.lineTo(-20, 130);
+    dr.lineTo(-20+tom.legangle*25, 130);
     dr.stroke();
     dr.beginPath();
     dr.moveTo(13, 85);
-    dr.lineTo(20, 130);
+    dr.lineTo(20-tom.legangle*25, 130);
     dr.stroke();
 
     dr.fillStyle="blue";
@@ -43,11 +54,11 @@ function draw(){
     dr.lineCap="round";
     dr.beginPath();
     dr.moveTo(-32, 20);
-    dr.lineTo(-52, 65);
+    dr.lineTo(-52-tom.armangle*20, 65);
     dr.stroke();
     dr.beginPath();
     dr.moveTo(32, 20);
-    dr.lineTo(52, 5);
+    dr.lineTo(52+tom.armangle*20, 65);
     dr.stroke();
 
     dr.fillStyle="white";
@@ -62,6 +73,7 @@ function draw(){
 function animate(){
     dr.clearRect(0,0,canv.width,canv.height);
     tom.y= canv.height-170;
+    walk();
     draw();
     requestAnimationFrame(animate);
 }
